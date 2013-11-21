851422488aa36fff7eed4f74256bd5c8aee77e04
liangjun.zhong <liangjun.zhong@dianping.com>
2013-11-21 15:44:05 +0800

    log

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.configure(function(){
   app.set('view engine', 'ejs');
   app.use(express.favicon());
   app.use(express.cookieParser());
-  app.use(express.logger('dev'));
+//  app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
