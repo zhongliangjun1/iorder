1aad4c376ef00911e2bb84214188fc92d696e476
liangjun.zhong <zhongliangjun1@gmail.com>
2013-11-30 02:46:30 +0800

    socket.io recommended production settings

--- app.js
+++ app.js
@@ -55,6 +55,18 @@ var server = http.createServer(app).listen(app.get('port'), function(){
 
 var io = socketio.listen(server);
 
+io.enable('browser client minification');  // send minified client
+io.enable('browser client etag');          // apply etag caching logic based on version number
+io.enable('browser client gzip');          // gzip the file
+io.set('log level', 1);                    // reduce logging
+io.set('transports', [
+    'websocket'
+    , 'flashsocket'
+    , 'htmlfile'
+    , 'xhr-polling'
+    , 'jsonp-polling'
+]);
+
 io.sockets.on('connection', function (socket) {
     console.log('connect once');
 
